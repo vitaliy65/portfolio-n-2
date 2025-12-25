e0e4f2d256b683f8b1a2c2d7ee0d8d90636201d2
vitaliy <vitaliy60606@gmail.com>
2025-12-25 14:25:00 +0200

    added sweater bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,18 @@ import SnowfallClient from "@/components/elements/SnowfallClient";
 export default function Home() {
   return (
     <main className="min-h-screen bg-background relative overflow-hidden">
-      <FloatingElements />
+      {/* Полупрозрачное изображение поверх фона с сохранением цвета, используя Tailwind CSS 4 */}
+      <div
+        className="pointer-events-none absolute inset-0 z-0 bg-[url('/img/bg-img.webp')] bg-repeat-y bg-[length:50%_auto] bg-left opacity-2"
+        style={{
+          backgroundPosition: "left top, right top",
+          backgroundRepeat: "repeat-y, repeat-y",
+          backgroundImage: "url('/img/bg-img.webp'), url('/img/bg-img.webp')",
+          backgroundSize: "50% auto, 50% auto"
+        }}
+        aria-hidden="true"
+      />
+      {/* <FloatingElements /> */}
       <Navigation />
 
       <div className="relative z-10">
